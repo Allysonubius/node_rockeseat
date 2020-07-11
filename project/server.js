3040626b2bfb44183d0a452ceaaef985e32314da
Allysonubius <allysomoliveira7@gmail.com>
2020-07-10 21:40:48 -0300

    Conectando Database MongoDB.

--- project/server.js
+++ project/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Init App
 const app = express();
 
+// Init DB
+mongoose.connect(
+    "mongodb://localhost:3001/node_api", {
+        useUnifiedTopology: true
+    }
+);
+
+// 1° route
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
+// Port
 app.listen(3001);
