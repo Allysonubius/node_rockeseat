74eed64e0cb71f7a865cae22c64f54c984eeaf89
Allysonubius <allysomoliveira7@gmail.com>
2020-07-11 13:35:31 -0300

    Criando models de products em API.

--- project/server.js
+++ project/server.js
@@ -17,15 +17,7 @@ requireDir('./src/models');
 const Product = mongoose.model('Product');
 
 // 1° route
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with',
-        url: 'https://github.com/facebook/react-native'
-    })
-
-    return res.send('Hello World')
-});
+app.use("/api", require("./src/route/routes"))
 
 // Port
 app.listen(3001);
